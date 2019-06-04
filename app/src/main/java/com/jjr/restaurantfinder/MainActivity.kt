80bc83d31b0d3a9c611abe120d7a3dc1edcba146
Loshgoobi <remy.dozoul@ynov.com>
2019-06-04 23:31:26 +0200

    ajout de la carte

--- app/src/main/java/com/jjr/restaurantfinder/MainActivity.kt
+++ app/src/main/java/com/jjr/restaurantfinder/MainActivity.kt
@@ -2,22 +2,20 @@ package com.jjr.restaurantfinder
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
-import android.support.v4.view.GravityCompat
-import android.support.v7.app.ActionBarDrawerToggle
-import android.view.MenuItem
-import android.support.v4.widget.DrawerLayout
 import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
 import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.content_main.*
 import java.io.IOException
-import java.util.ArrayList
+import java.util.*
 
 class MainActivity : AppCompatActivity(), ImageRequester.ImageRequesterResponse, NavigationView.OnNavigationItemSelectedListener  {
 
@@ -41,11 +39,7 @@ class MainActivity : AppCompatActivity(), ImageRequester.ImageRequesterResponse,
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val toggle = ActionBarDrawerToggle(
@@ -133,7 +127,8 @@ class MainActivity : AppCompatActivity(), ImageRequester.ImageRequesterResponse,
 
             }
             R.id.nav_slideshow -> {
-
+                val intent = Intent(this@MainActivity, MapsActivity::class.java)
+                startActivity(intent)
             }
             R.id.nav_tools -> {
 
